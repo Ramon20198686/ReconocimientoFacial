06386fc7e2fb86a186e119f216e995da8d35a3e5
Ramon20198686 <20198686@itla.edu.do>
2021-04-29 16:33:23 -0400

    Conexion a DB

---
+++ ProyectoFinal/Controllers/HomeController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CONTROL_DOCUMENTOSEntities db = new CONTROL_DOCUMENTOSEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var uSUARIOS = db.USUARIOS.Include(u => u.DEPARTAMENTOS);
+            return View(uSUARIOS.ToList());
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            if (uSUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIOS);
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            ViewBag.DEPARTAMENTO = new SelectList(db.DEPARTAMENTOS, "SIGLAS", "NOMBRE");
+            return View();
+        }
+
+        // POST: Home/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CODIGO,NOMBRE,CORREO,DEPARTAMENTO,CARGO")] USUARIOS uSUARIOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.USUARIOS.Add(uSUARIOS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DEPARTAMENTO = new SelectList(db.DEPARTAMENTOS, "SIGLAS", "NOMBRE", uSUARIOS.DEPARTAMENTO);
+            return View(uSUARIOS);
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            if (uSUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DEPARTAMENTO = new SelectList(db.DEPARTAMENTOS, "SIGLAS", "NOMBRE", uSUARIOS.DEPARTAMENTO);
+            return View(uSUARIOS);
+        }
+
+        // POST: Home/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CODIGO,NOMBRE,CORREO,DEPARTAMENTO,CARGO")] USUARIOS uSUARIOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uSUARIOS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DEPARTAMENTO = new SelectList(db.DEPARTAMENTOS, "SIGLAS", "NOMBRE", uSUARIOS.DEPARTAMENTO);
+            return View(uSUARIOS);
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            if (uSUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIOS);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            db.USUARIOS.Remove(uSUARIOS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        //NUEVO
+
+
+        // GET: Home/CreateDep
+        public ActionResult CreateDep()
+        {
+            ViewBag.DEPARTAMENTO = new SelectList(db.DEPARTAMENTOS, "SIGLAS", "NOMBRE");
+            return View();
+        }
+
+        // POST: Home/CreateDep
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateDep([Bind(Include = "SIGLAS,NOMBRE")] DEPARTAMENTOS dEPARTAMENTOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DEPARTAMENTOS.Add(dEPARTAMENTOS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DEPARTAMENTO = new SelectList(db.DEPARTAMENTOS, "SIGLAS", "NOMBRE", dEPARTAMENTOS);
+            return View(dEPARTAMENTOS);
+        }
+
+        // GET: Home/DetailsDep/5
+        public ActionResult DetailsDep(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DEPARTAMENTOS dEPARTAMENTOS = db.DEPARTAMENTOS.Find(id);
+            if (dEPARTAMENTOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dEPARTAMENTOS);
+        }
+
+    }
+}
